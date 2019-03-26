fe919bf88d24cd31662f1d7b738c2935cb85398b
桃园令 <whymaths@gmail.com>
2019-03-26 17:38:33 +0800

    Update 192.wordcount.sh

--- 192.wordcount.sh
+++ 192.wordcount.sh
@@ -1,1 +1,3 @@
 cat words.txt | tr ' ' '\n' | tr -s '\n' | sort | uniq -c | sort -rn | awk '{print $2,$1}'
+
+# 一位前人的答案：cat words.txt | tr -s ' ' '\n' | sort | uniq -c | sort -r | awk '{ print $2, $1 }'
